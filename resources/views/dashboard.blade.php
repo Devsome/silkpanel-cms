038a3f5ef9127c29b5f5662ce46469f97bbe2406
Devsome <info@devsome.com>
2026-04-28 01:57:48 +0200

    fix: remove commented-out referral system section from dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -211,7 +211,6 @@
                 </div>
             @endif
 
-            {{-- Referral System (only if enabled) --}}
             @if ($referralEnabled && $referralData)
                 <div
                     class="bg-white dark:bg-gray-800 rounded-lg shadow-sm border border-gray-200 dark:border-gray-700 p-6">
